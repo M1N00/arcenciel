493ecb9f20f065f7149be1e03f07db42cf8b4066
M1N00 <antoni.debaz@gmail.com>
2025-02-27 19:16:29 +0100

    Add files via upload

--- activité.js
+++ activité.js
@@ -1,37 +1,119 @@
+//Bonjour Véronique, vous pouvez inséré le titre de l'activité et la description ici entre les guillemets
+var TITRE = "Ceci est un example de titre d'activité"
+var DESCRIPTION = "Ceci est un example de description d'activité"
 
 
-// Configuration Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyB9xEmB-83GBS5fDRrmo7zPBWhReC3QkkE",
-    authDomain: "backend-jardin-enfant.firebaseapp.com",
-    projectId: "backend-jardin-enfant",
-    storageBucket: "backend-jardin-enfant.firebasestorage.app",
-    messagingSenderId: "840177966004",
-    appId: "1:840177966004:web:c265a2b1477356ec6570e1"
-};
 
-// Initialisation de Firebase
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
 
-// Accès à Firestore
-const db = firebase.firestore();
 
-// Fonction pour récupérer et imprimer le titre
-function fetchActivityTitle() {
-    db.collection("Activities").doc("montagne").get()
-        .then((doc) => {
-            if (doc.exists) {
-                console.log("Titre:", doc.data().titre);
-            } else {
-                console.log("Aucun document trouvé !");
-            }
-        })
-        .catch((error) => {
-            console.error("Erreur lors de la récupération du document:", error);
-        });
-}
 
-// Appel de la fonction avec un délai de 2 secondes
-setTimeout(fetchActivityTitle, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Titre = document.getElementById("Titre")
+var Description = document.getElementById("DESCRIPTION")
+//var image = document.getElementById("IMAGE")
+
+Titre.innerHTML = TITRE
+Description.innerHTML = DESCRIPTION
+//image.src = "Peinture1.PNG";
